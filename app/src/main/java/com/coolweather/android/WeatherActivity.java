8cb5ea8c7289192009e7270392a1f45cdf83692a
VIP-bear <1907464379@qq.com>
2018-11-22 21:02:36 +0800

    增加了后台自动更新天气的功能。

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.coolweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -216,6 +217,10 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
+
     }
 
     /**
@@ -252,5 +257,6 @@ public class WeatherActivity extends AppCompatActivity {
             }
         });
     }
+
 
 }
